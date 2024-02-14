dd5c78086fcea6cd44e5555395efb82e06c9edf1
mullzalt <rizki.d.mulya@gmail.com>
2024-02-14 11:58:52 +0700

    add transparent-nvim

---
+++ lua/plugins/transparent-nvim.lua
@@ -0,0 +1,19 @@
+local prefix = "<leader>u"
+return {
+	"xiyaowong/transparent.nvim",
+	lazy = false,
+	config = function()
+		local transparent = require("transparent")
+		transparent.setup({
+			extra_groups = {
+				"NormalFloat",
+				"NvimTreeNormal",
+			},
+		})
+		transparent.clear_prefix("BufferLine")
+		transparent.clear_prefix("lualine")
+	end,
+	keys = {
+		{ prefix .. "T", "<cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
+	},
+}
