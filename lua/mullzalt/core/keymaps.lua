75bb92ad64d0fdc25d10d0391b5156e5123652c6
mullzalt <rizki.d.mulya@gmail.com>
2024-05-26 11:55:44 +0700

    lspconfig

--- lua/mullzalt/core/keymaps.lua
+++ lua/mullzalt/core/keymaps.lua
@@ -3,23 +3,22 @@ vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
 
-
 -- move line in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down", expr = true, silent = true})
-vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selected line down", expr = true, silent = true})
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up", expr = true, silent = true})
-vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selected line up", expr = true, silent = true})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down", expr = true, silent = true })
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selected line down", expr = true, silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up", expr = true, silent = true })
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selected line up", expr = true, silent = true })
 
 -- join line
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line", silent = true})
-vim.keymap.set("n", "<S-Down>", "mzJ`z", { desc = "Join line", silent = true})
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line", silent = true })
+vim.keymap.set("n", "<S-Down>", "mzJ`z", { desc = "Join line", silent = true })
 
 -- better scroll
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down", silent = true})
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up", silent = true})
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down", silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up", silent = true })
 
-vim.keymap.set("n", "n", "nzzzv", { desc = "Find next matching patern", silent = true})
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Find previous matching patern", silent = true})
+vim.keymap.set("n", "n", "nzzzv", { desc = "Find next matching patern", silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Find previous matching patern", silent = true })
 
 -- toggle wrap
 vim.keymap.set("n", "<leader>ww", "<cmd>set wrap!<cr>", { desc = "Toggle text wrap", remap = true })
@@ -32,13 +31,14 @@ vim.keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true,
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "x", '"_x')
+-- vim.keymap.set({ "n", "v" }, "x", '"_x')
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank selected to clipboard"})
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank current line to clipboard"})
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank selected to clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank current line to clipboard" })
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to unregister" })
+vim.keymap.set({ "n", "v" }, "<leader>x", '"_x', { desc = "Delete under cursor to unregister" })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
--- lua/mullzalt/plugins/which-key.lua
+++ lua/mullzalt/plugins/which-key.lua
@@ -1,21 +1,26 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    defaults = {
-      ["<leader>f"] = {name = "+  Find Files"},
-      ["<leader>h"] = {name = "+  Harpoon"},
-      ["<leader>t"] = {name = "+  Troubles"},
-      ["<leader>w"] = {name = "+ 󰖶 Toggle Wrap"},
-    }
-  },
-  config = function(_, opts)
-    local wk = require("which-key")
-    wk.setup(opts)
-    wk.register(opts.defaults)
-  end,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		defaults = {
+			["<leader>f"] = { name = " Find Files" },
+			["<leader>h"] = { name = " Harpoon" },
+			["<leader>t"] = { name = " Troubles" },
+			["<leader>w"] = { name = "󰖶 Wrap" },
+			["<leader>d"] = { name = "󰍉 Diagnostic" },
+			["<leader>s"] = { name = "  Session" },
+			["<leader>r"] = { name = " LSP Actions" },
+			["<leader>c"] = { name = " Code Actions" },
+			["<leader>m"] = { name = "󰉼 Modify buffer" },
+		},
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.register(opts.defaults)
+	end,
 }
