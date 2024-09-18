f308efaf1d569af9e3df840f68b00beda8b907d2
mullzalt <rizki.d.mulya@gmail.com>
2024-09-18 16:54:44 +0700

    update some conflicting script

--- lua/mullzalt/plugins/lspconfig.lua
+++ lua/mullzalt/plugins/lspconfig.lua
@@ -87,6 +87,10 @@ return {
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
+		lspconfig["gdscript"].setup({
+			capabilities = capabilities,
+		})
+
 		mason_lspconfig.setup_handlers({
 			-- default handler for installed servers
 			function(server_name)
